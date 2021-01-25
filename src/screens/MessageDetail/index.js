bba05af9468362c24550f0be1fea278c0f095212
Roni Castro <dextra.rcesar@creditas.com>
2021-01-25 02:32:57 -0300

    feat: create base MessageDetail screen

---
+++ src/screens/MessageDetail/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+export const MessageDetail = () => {
+  return (
+    <View>
+      <Text>Message Detail</Text>
+    </View>
+  );
+};
