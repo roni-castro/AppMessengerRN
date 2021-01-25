e65d0fee6e09e3d3c27bb8f3b4c2322e553ef3b7
Roni Castro <dextra.rcesar@creditas.com>
2021-01-25 18:00:52 -0300

    feat: add loading to Home screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,21 +1,38 @@
 import React, {useEffect, useState} from 'react';
-import {FlatList} from 'react-native';
+import {ActivityIndicator, FlatList} from 'react-native';
 import {Header} from '../../components/Header';
 import MessageCard from '../../components/MessageCard';
 import {Container} from './styles';
 
 const Home = ({navigation}) => {
   const [messages, setMessages] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
 
   useEffect(() => {
     function fetchMessages() {
+      setisLoading(true);
       fetch('https://appmessenger-api.herokuapp.com/message')
         .then((response) => response.json())
         .then((data) => setMessages(data))
-        .catch((e) => console.log(e));
+        .catch((e) => console.log(e))
+        .finally(() => setisLoading(false));
     }
     fetchMessages();
   }, []);
+
+  if (isLoading) {
+    return (
+      <ActivityIndicator
+        size="large"
+        color="#0000FF"
+        style={{
+          flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      />
+    );
+  }
 
   return (
     <Container>
