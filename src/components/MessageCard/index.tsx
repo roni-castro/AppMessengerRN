49bcb50b05804fdf5b64e58c27378fae1ff9d055
Roni Castro <dextra.rcesar@creditas.com>
2021-01-25 02:02:54 -0300

    feat: finish MessageCard style

--- src/components/MessageCard/index.tsx
+++ src/components/MessageCard/index.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
 import moment from 'moment';
 import Divider from '../Divider';
 import {H2, ParagraphLight, ParagraphNormal} from '../Typography';
 import {IMessageData} from '../models/messages';
+import {Container, TitleContainer} from './styles';
 
 interface IMessageCardProps {
   item: IMessageData;
 }
 
 const MessageCard = ({item}: IMessageCardProps) => (
-  <TouchableOpacity onPress={() => console.warn(`clickou ${item.id}`)}>
-    <H2>{item.subject}</H2>
-    <ParagraphLight>
-      {moment(new Date(item.timestamp * 1000)).fromNow()}
-    </ParagraphLight>
+  <Container onPress={() => console.warn(`clickou ${item.id}`)}>
+    <TitleContainer>
+      <H2 style={{width: '70%'}} numberOfLines={1} ellipsizeMode="tail">
+        {item.subject}
+      </H2>
+      <ParagraphLight>
+        {moment(new Date(item.timestamp * 1000)).fromNow()}
+      </ParagraphLight>
+    </TitleContainer>
     <ParagraphNormal numberOfLines={2} ellipsizeMode="tail">
       {item.detail}
     </ParagraphNormal>
     <Divider />
-  </TouchableOpacity>
+  </Container>
 );
 
 export default MessageCard;
---
+++ src/components/MessageCard/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  margin-horizontal: 20px;
+  margin-vertical: 10px;
+`;
+
+export const TitleContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 10px;
+`;
